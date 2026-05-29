86f2667e8da1d9945c8d0e081916160b7aa59dd5
Chris Martinez <sydefekt@hotmail.com>
2026-05-29 14:38:11 -0700

    Refactor extensions into prelude

--- src/lib.rs
+++ src/lib.rs
@@ -8,35 +8,42 @@ pub use inject_keyed::{
     InjectAllWithKey, InjectAllWithKeyMut, InjectWithKey, InjectWithKeyMut, TryInjectWithKey, TryInjectWithKeyMut,
 };
 
-async fn services_middleware(
-    State(provider): State<ServiceProvider>,
-    mut request: Request,
-    next: Next,
-) -> Response {
-    request.extensions_mut().insert(provider.create_scope());
-    next.run(request).await
-}
+/// Contains library prelude.
+pub mod prelude {
+    use axum::{
+        extract::{Request, State},
+        middleware::{from_fn_with_state, Next},
+        response::Response,
+        Router,
+    };
+    use di::ServiceProvider;
 
-/// Provides [`axum::Router`] extension methods.
-pub trait RouterServiceProviderExtensions {
-    /// Adds the specified service provider to a router.
-    ///
-    /// # Arguments
-    ///
-    /// * `provider` - the [`di::ServiceProvider`] applied to the router
-    ///
-    /// # Remarks
-    ///
-    /// The service provider should be added after all routes are defined
-    /// in the same manner as middleware.
-    fn with_provider(self, provider: ServiceProvider) -> Self;
-}
+    async fn services_middleware(
+        State(provider): State<ServiceProvider>,
+        mut request: Request,
+        next: Next,
+    ) -> Response {
+        request.extensions_mut().insert(provider.create_scope());
+        next.run(request).await
+    }
 
-impl<S> RouterServiceProviderExtensions for Router<S>
-where
-    S: Clone + Send + Sync + 'static,
-{
-    fn with_provider(self, provider: ServiceProvider) -> Self {
-        self.route_layer(from_fn_with_state(provider, services_middleware))
+    /// Provides [router][Router] extension methods.
+    pub trait RouterExt: Sized {
+        /// Adds the specified service provider to a router.
+        ///
+        /// # Arguments
+        ///
+        /// * `provider` - the [service provider][ServiceProvider] applied to the router
+        ///
+        /// # Remarks
+        ///
+        /// The service provider should be added after all routes are defined in the same manner as middleware.
+        fn with_provider(self, provider: ServiceProvider) -> Self;
+    }
+
+    impl<S: Clone + Send + Sync + 'static> RouterExt for Router<S> {
+        fn with_provider(self, provider: ServiceProvider) -> Self {
+            self.route_layer(from_fn_with_state(provider, services_middleware))
+        }
     }
 }
